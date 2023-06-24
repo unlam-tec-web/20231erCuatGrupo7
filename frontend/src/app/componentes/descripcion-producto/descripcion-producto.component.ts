3e672715b85a20e753ad5d73da5f3bc6fc1c5dd1
1cristianb <crboschi@alumno.unlam.edu.ar>
2023-06-24 00:06:38 -0300

    frontend

---
+++ frontend/src/app/componentes/descripcion-producto/descripcion-producto.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-descripcion-producto',
+  templateUrl: './descripcion-producto.component.html',
+  styleUrls: ['./descripcion-producto.component.css']
+})
+export class DescripcionProductoComponent {
+
+}
