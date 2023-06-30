ab6e94cb5cfb9fd5f938bffcfd406f839341cccf
1cristianb <crboschi@alumno.unlam.edu.ar>
2023-06-30 20:51:06 -0300

    se modificó backend

--- servidor/src/routes/productosRoutes.js
+++ servidor/src/routes/productosRoutes.js
@@ -1,17 +1,67 @@
 import { Router } from 'express';
-import { getProductos, createProducto, updateProducto, deleteProducto, getUnProducto, getProductosComic, getProductosManga, getProductosLibro } from '../controllers/productosController.js';
+import conexion from '../db.js';
 
 const router = Router();
 
-router.get('/productos', getProductos);
 
-router.get('/productos/comic', getProductosComic);
-router.get('/productos/libro', getProductosLibro);
-router.get('/productos/manga', getProductosManga);
+router.get('/productos/comic', (req, res) => {
+    let sql = "select * from productos where clasificacion='comic' or clasificacion='Comic'";
+    conexion.query(sql, (err, rows, fields) => {
+        if (err) throw err;
+        else {
+            res.json(rows)
+        }
+    })
 
-router.get('/productos/:id', getUnProducto);
+})
 
-router.post('/productos', createProducto);
+router.get('/productos/libro', (req, res) => {
+    let sql = "select * from productos where clasificacion='libro' or clasificacion='Libro'";
+    conexion.query(sql, (err, rows, fields) => {
+        if (err) throw err;
+        else {
+            res.json(rows)
+        }
+    })
+
+})
+
+router.get('/productos/manga', (req, res) => {
+    let sql = "select * from productos where clasificacion='manga' or clasificacion='Manga'";
+    conexion.query(sql, (err, rows, fields) => {
+        if (err) throw err;
+        else {
+            res.json(rows)
+        }
+    })
+
+})
+router.get('/productos/:id', (req, res) => {
+    let { id } = req.params;
+    let sql = `SELECT * FROM productos WHERE id = ${id}`;
+    conexion.query(sql, (error, result) => {
+        if (error) throw error;
+
+        if (result.length > 0) {
+            res.json(result[0]);
+        } else {
+            res.send('Not result');
+        }
+    });
+});
+
+
+router.post('/productos', (req, res) => {
+    const { nombre, descripcion, clasificacion, precio, imagen } = req.body
+
+    let sql = `insert into productos( nombre,descripcion,clasificacion,precio,imagen) values('${nombre}','${descripcion}','${clasificacion}','${precio}','${imagen}')`
+    conexion.query(sql, (err, rows, fields) => {
+        if (err) throw err
+        else {
+            res.json({ status: 'equipo agregado' })
+        }
+    })
+})
 
 
 export default router;
